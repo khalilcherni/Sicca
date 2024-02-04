45d06f622ac6af07ebf69bffeb480e34ff22be3a
khalilcherni <kcherni411@gmail.com>
2024-02-04 13:57:00 +0100

    51

--- Frontend/my-app/src/components/Details.jsx
+++ Frontend/my-app/src/components/Details.jsx
@@ -4,7 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Details() {
   return (
     <div>
-     
+        <Card style={{ width: '18rem' }} className="custom-card">
+                <Card.Img variant="top" src={e.image_url} />
+                <Card.Body>
+                  <Card.Title>{e.name}</Card.Title>
+                  <Card.Text>{e.description}</Card.Text>
+                  
+                  </Card.Body>
+              </Card>
     </div>
   )
 }
