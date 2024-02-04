3d9927980b3d11df37f1ce43b5fbb3fc2a22141e
khalilcherni <kcherni411@gmail.com>
2024-02-04 14:49:47 +0100

    54

--- backend/routes/tourismRoutes.js
+++ backend/routes/tourismRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const tourismController = require("../Controllers/tourismController");
 
 router.get("/getAll", tourismController.getAllTourism);
-router.get("/:place_id", tourismController.getOneTourism);
+router.get("/:name", tourismController.getOneTourism);
 router.post("/add", tourismController.addTourism);
 router.put("/update/:place_id", tourismController.updateTourism);
 router.delete("/delete/:place_id", tourismController.deleteTourism);
