2f6fcae6aa93b61d775324bef514818ba440204e
guelmamihiba <guelmamihiba1@gmail.com>
2024-02-04 15:33:12 +0100

    buttons

--- Frontend/my-app/src/components/Culture.jsx
+++ Frontend/my-app/src/components/Culture.jsx
@@ -2,34 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'; // Add this import
-import Details from "./Details"
+import Button from 'react-bootstrap/Button';
+import Details from "./Details";
 import video from "../video/El Kef Tunisia تونس الكاف (1).mp4";
 import './Culture.css';
 import StarRating from './StarRating';
 
-
 function Places() {
   const [data, setData] = useState([]);
   const [editingPlaceId, setEditingPlaceId] = useState(null);
- 
+
   const [updatedDescription, setUpdatedDescription] = useState('');
   const [updatedImageUrl, setUpdatedImageUrl] = useState('');
-  const [updatecategory,setcategory]=useState("")
-  const[updatelocation,setloc]=useState("")
-  const[updatecontac,setcona]=useState("")
-
+  const [updatecategory, setcategory] = useState('');
+  const [updatelocation, setloc] = useState('');
+  const [updatecontac, setcona] = useState('');
 
   const [menuView, setMenuView] = useState(false);
   const [view, setView] = useState('Culture');
-  const [product,setproduct]=useState(null)
- 
+  const [product, setproduct] = useState(null);
 
   const switchView = (option, place = null) => {
     setView(option);
     setproduct(place);
   };
-  
+
   useEffect(() => {
     axios.get('http://localhost:3001/cultural/getAll')
       .then(res => setData(res.data))
@@ -57,17 +54,15 @@ function Places() {
 
     switchView('Details', placeToUpdate);  // Switch the view to 'Details' and pass the place
   };
-  
+
   const handleUpdateSubmit = (placeId) => {
     axios
       .put(`http://localhost:3001/cultural/update/${placeId}`, {
-       
         image_url: updatedImageUrl,
         description: updatedDescription,
         cultural_category: updatecategory,
-        location:updatelocation,
-        contact_info:updatecontac
-
+        location: updatelocation,
+        contact_info: updatecontac
       })
       .then((response) => {
         console.log(response.data);
@@ -75,24 +70,25 @@ function Places() {
           item.place_id === placeId
             ? {
                 ...item,
-             image_url: updatedImageUrl,
-        description: updatedDescription,
-        cultural_category: updatecategory,
-        location:updatelocation,
-        contact_info:updatecontac
+                image_url: updatedImageUrl,
+                description: updatedDescription,
+                cultural_category: updatecategory,
+                location: updatelocation,
+                contact_info: updatecontac
               }
             : item
         );
         setData(updatedData);
         setEditingPlaceId(null);
-        setproduct(null)
+        setproduct(null);
       })
       .catch((err) => console.error('Error updating place:', err));
   };
-  
+
   const handleCancelUpdate = () => {
     setEditingPlaceId(null);
-  }
+  };
+
   const handleRatingClick = (clickedRating, placeId) => {
     setData(prevData => prevData.map(e => (e.place_id === placeId ? { ...e, rating: clickedRating } : e)));
   };
@@ -113,65 +109,58 @@ function Places() {
                   <Card.Text>{e.description}</Card.Text>
                   <StarRating rating={e.rating} onRatingClick={(clickedRating) => handleRatingClick(clickedRating, e.place_id)} />
                   <div>
-                  {editingPlaceId === e.place_id ? (
-                <div>
-                 
-                  <label htmlFor="newImage">New Image URL:</label>
-                  <input
-                    type="text"
-                    id="newImage"
-                    value={updatedDescription}
-                    onChange={(e) => setUpdatedDescription(e.target.value)}
-                  />
-                  <label htmlFor="newDescription">New Description:</label>
-                  <input
-                    type="text"
-                    id="newDescription"
-                    value={updatedImageUrl}
-                    onChange={(e) => setUpdatedImageUrl(e.target.value)}
-                  />
-                    <label htmlFor="newDescription">New Description:</label>
-                  <input
-                    type="text"
-                    id="newDescription"
-                    value={updatelocation}
-                    onChange={(e) => setloc(e.target.value)}
-                    
-                  />
-                    <label htmlFor="newDescription">New Description:</label>
-                  <input
-                    type="text"
-                    id="newDescription"
-                    value={updatecategory}
-                    onChange={(e) => setcategory(e.target.value)}
-                  />
-                    <label htmlFor="newDescription">New Description:</label>
-                  <input
-                    type="text"
-                    id="newDescription"
-                    value={updatecontac}
-                    onChange={(e) => setcona(e.target.value)}
-                  />
-               
-                </div>
-                
-              ) : (
-                <Card.Text>{e.description}</Card.Text>
-              )}
-          
-              {editingPlaceId === e.place_id ? (
-                <div>
-                  <Button variant="success" id='button-89' onClick={() => handleUpdateSubmit(e.place_id)}>Update</Button>
-                  <Button variant="danger"  id='button-89' onClick={handleCancelUpdate}>Cancel</Button>
-                </div>
-              ) : (
-                <div>
-                  <Button variant="primary"  id='button-89' onClick={() => handleDeleteClick(e.place_id)}>Delete</Button>
-                  <Button variant="secondary"  id='button-89' onClick={() => handleUpdateClick(e.place_id)}>Update</Button>
-                </div>
+                    {editingPlaceId === e.place_id ? (
+                      <div>
+                        <label htmlFor="newImage">New Image URL:</label>
+                        <input
+                          type="text"
+                          id="newImage"
+                          value={updatedImageUrl}
+                          onChange={(e) => setUpdatedImageUrl(e.target.value)}
+                        />
+                        <label htmlFor="newDescription">New Description:</label>
+                        <input
+                          type="text"
+                          id="newDescription"
+                          value={updatedDescription}
+                          onChange={(e) => setUpdatedDescription(e.target.value)}
+                        />
+                        <label htmlFor="newLocation">New Location:</label>
+                        <input
+                          type="text"
+                          id="newLocation"
+                          value={updatelocation}
+                          onChange={(e) => setloc(e.target.value)}
+                        />
+                        <label htmlFor="newCategory">New Category:</label>
+                        <input
+                          type="text"
+                          id="newCategory"
+                          value={updatecategory}
+                          onChange={(e) => setcategory(e.target.value)}
+                        />
+                        <label htmlFor="newContact">New Contact:</label>
+                        <input
+                          type="text"
+                          id="newContact"
+                          value={updatecontac}
+                          onChange={(e) => setcona(e.target.value)}
+                        />
+                      </div>
+                    ) : (
+                      <div>
+                        <Button variant="primary" id='button-89' onClick={() => handleDeleteClick(e.place_id)}>Delete</Button>
+                        <Button variant="secondary" id='button-89' onClick={() => handleUpdateClick(e.place_id)}>Update</Button>
+                      </div>
                     )}
                   </div>
                 </Card.Body>
+                {editingPlaceId === e.place_id && (
+                  <Card.Footer>
+                    <Button variant="success" id='button-89' onClick={() => handleUpdateSubmit(e.place_id)}>Update</Button>
+                    <Button variant="danger" id='button-89' onClick={handleCancelUpdate}>Cancel</Button>
+                  </Card.Footer>
+                )}
               </Card>
             </div>
           ))}
