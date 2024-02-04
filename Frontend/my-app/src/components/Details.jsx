f819ae3dd9100e19257539eae8aa6389dd7d4da3
khalilcherni <kcherni411@gmail.com>
2024-02-04 13:56:46 +0100

    50

---
+++ Frontend/my-app/src/components/Details.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Details() {
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
+export default Details
